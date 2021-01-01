47520b143f2ca87dcd3bc64b36d66e01e3130a97
Shivam Kushwaha <shivamk8368@gmail.com>
2021-01-01 09:57:45 -0500

    "adding new host"

--- tubers/settings.py
+++ tubers/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ce205kzxsuj_axwq&o)^%jnwqz=g12^zxwjz@k*p5_iaor!=to'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lco-tubers.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['lco-tubers.herokuapp.com', '127.0.0.1', 'ytubers.net']
 LOGIN_REDIRECT_URL = 'dashboard'
 
 
